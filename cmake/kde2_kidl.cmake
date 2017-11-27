57f5a9f05852068dafbfb2faac773b5c686b4979
Helio Chissini de Castro <helio@kde.org>
2017-11-27 07:58:39 +0100

    - Start to add kdeprint - Update kde2_kidl function. Besides improved, still not happy with the results

--- cmake/kde2_kidl.cmake
+++ cmake/kde2_kidl.cmake
@@ -1,27 +1,29 @@
-macro(KDE2_KIDL Name Input)
-    get_filename_component(FILENAME ${Input} NAME_WE)
-    get_filename_component(FULLFILENAME ${Input} ABSOLUTE)
+macro(KDE2_KIDL idlfiles)
+    foreach(Input ${idlfiles})
+        get_filename_component(FILENAME ${Input} NAME_WE)
+        get_filename_component(FULLFILENAME ${Input} ABSOLUTE)
 
-    message(STATUS "${FILENAME} ${FULLFILENAME}")
+        message(STATUS "${FILENAME} ${FULLFILENAME}")
 
-    add_custom_command(OUTPUT ${FILENAME}.kidl
-        COMMAND dcopidl ${FULLFILENAME} > ${FILENAME}.kidl
-        VERBATIM
-        DEPENDS ${FULLFILENAME}
-        COMMENT "Generating KIDL for ${Input}"
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        )
-    add_custom_command(OUTPUT ${FILENAME}_skel.cpp
-        COMMAND dcopidl2cpp --c++-suffix cpp --no-stub ${FILENAME}.kidl
-        VERBATIM
-        DEPENDS ${FILENAME}.kidl
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        )
-    add_custom_command(OUTPUT ${FILENAME}_stub.cpp
-        COMMAND dcopidl2cpp --c++-suffix cpp --no-skel ${FILENAME}.kidl
-        VERBATIM
-        DEPENDS ${FILENAME}.kidl
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        )
+        add_custom_command(OUTPUT ${FILENAME}.kidl
+            COMMAND dcopidl ${FULLFILENAME} > ${FILENAME}.kidl
+            VERBATIM
+            DEPENDS ${FULLFILENAME}
+            COMMENT "Generating KIDL for ${Input}"
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            )
+        add_custom_command(OUTPUT ${FILENAME}_skel.cpp
+            COMMAND dcopidl2cpp --c++-suffix cpp --no-stub ${FILENAME}.kidl
+            VERBATIM
+            DEPENDS ${FILENAME}.kidl
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            )
+        add_custom_command(OUTPUT ${FILENAME}_stub.cpp
+            COMMAND dcopidl2cpp --c++-suffix cpp --no-skel ${FILENAME}.kidl
+            VERBATIM
+            DEPENDS ${FILENAME}.kidl
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            )
+    endforeach()
 endmacro()
 
